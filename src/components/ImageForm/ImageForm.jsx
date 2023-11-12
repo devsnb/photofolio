8d10ce75b1f5588e94f63f2e74e527b242c06572
Snehangshu Biswas <59432533+devsnb@users.noreply.github.com>
2023-11-12 11:23:20 +0530

    Update ImageForm.jsx

--- src/components/ImageForm/ImageForm.jsx
+++ src/components/ImageForm/ImageForm.jsx
@@ -3,7 +3,7 @@ import { doc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-import { db } from '../../firebaseInit'
+import { db } from '../../firebaseinit'
 
 import styles from './ImageForm.module.css'
 
